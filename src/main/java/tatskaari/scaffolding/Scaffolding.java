cc1a858afb6b4840dbb831c5caee27a728375505
Jonathan <jonfpoole@gmail.com>
2016-12-28 22:39:51 +0000

    Added recipe for scaffodling

--- src/main/java/tatskaari/scaffolding/Scaffolding.java
+++ src/main/java/tatskaari/scaffolding/Scaffolding.java
@@ -3,8 +3,10 @@ package tatskaari.scaffolding;
 import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -26,7 +28,10 @@ public class Scaffolding {
         GameRegistry.register(ITEM_SCAFFOLD);
 
         ModelResourceLocation scaffoldingItemModelResLoc = new ModelResourceLocation(MODID+":"+"scaffold", "inventory");
-        Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(ITEM_SCAFFOLD, 0, scaffoldingItemModelResLoc);
+        Minecraft.getMinecraft().getRenderItem().getItemModelMesher()
+                .register(ITEM_SCAFFOLD, 0, scaffoldingItemModelResLoc);
+
+        GameRegistry.addSmelting(Items.REEDS, new ItemStack(ITEM_SCAFFOLD, 4), 0.35f);
 
     }
 }
--- src/main/java/tatskaari/scaffolding/items/ItemScaffold.java
+++
@@ -1,7 +0,0 @@
-package tatskaari.scaffolding.items;
-
-/**
- * Created by Tatskaari on 27/12/2016.
- */
-public class ItemScaffold {
-}
